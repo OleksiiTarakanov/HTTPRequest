109e522c78521031757d47cd26eb2ae278144c55
Oleksii Tarakanov <SiriCuster@gmail.com>
2020-04-06 16:30:02 +0300

    async works

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-(function(){
-    document.querySelector('.search').addEventListener('click',function(){
-        let fSearch = document.forms['bookSearch'];
-        let inpVal = fSearch.tr.value;
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET',`https://it-ebooks-api.info/v1/search/${inpVal}`);
-        document.querySelector('.text').innerHTML += xhr.responseText;
+(function () {
+  document.querySelector('.search').addEventListener('click', function () {
+
+    let fSearch = document.forms['bookSearch'];
+    let inpVal = fSearch.tr.value;
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', `https://api.itbook.store/1.0/search/${inpVal}`);
+    xhr.onload = () => {
+      if (xhr.readyState == 4 && xhr.status == 200) {
         console.log(xhr.status);
-        console.log(inpVal);
-        xhr.send();
-        fSearch.reset();
-        
-    });
+        document.querySelector('.text').innerHTML = xhr.responseText;
+      } 
+    }
+    console.log(xhr.status);
+    console.log(inpVal);
+    xhr.send();
+    fSearch.reset();
+
+  });
 })();
